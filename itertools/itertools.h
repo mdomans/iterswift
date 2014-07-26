733ed03e0da51c5fe074d8a2661b38de9b9ad9b6
mdomans <michaldoman@k2.pl>
2014-07-26 17:04:03 +0200

    Initial Commit

---
+++ itertools/itertools.h
@@ -0,0 +1,19 @@
+//
+//  itertools.h
+//  itertools
+//
+//  Created by mdomans on 26/07/14.
+//  Copyright (c) 2014 mdomans. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for itertools.
+FOUNDATION_EXPORT double itertoolsVersionNumber;
+
+//! Project version string for itertools.
+FOUNDATION_EXPORT const unsigned char itertoolsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <itertools/PublicHeader.h>
+
+
